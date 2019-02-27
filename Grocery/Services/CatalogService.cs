d3a6d30bd48c3e4dff6ddc31b46b6f955b361483
Ilya Vasilyev <ivasilyev@servicetitan.com>
2019-02-27 16:15:26 +1000

    implemented CatalogService

--- Grocery/Services/CatalogService.cs
+++ Grocery/Services/CatalogService.cs
@@ -13,24 +13,56 @@ namespace Grocery.Services
         private Dictionary<string, PricePerItem> _pricesPerItem = new Dictionary<string, PricePerItem>();
         private Dictionary<string, BulkDiscount> _bulkDiscounts = new Dictionary<string, BulkDiscount>();
 
+        /// <summary>
+        /// Returns bulk discount for item or null if no discount for the item exists
+        /// </summary>
+        /// <returns>Bulk discount or null if not found</returns>
         public BulkDiscount GetBulkDiscount(string name)
         {
-            throw new NotImplementedException();
+            if (_bulkDiscounts.ContainsKey(name))
+             return _bulkDiscounts[name];
+
+            return null;
         }
 
+        /// <summary>
+        /// Returns price of the item
+        /// </summary>
+        /// <param name="name">Item name</param>
+        /// <returns>Price</returns>
         public decimal GetItemPrice(string name)
         {
-            throw new NotImplementedException();
+            if (_pricesPerItem.ContainsKey(name))
+                return _pricesPerItem[name].Price;
+
+            throw new InvalidOperationException($"Can't find price for {name} item");
         }
 
+        /// <summary>
+        /// Registers price for the item
+        /// </summary>
+        /// <param name="item">Item</param>
+        /// <param name="price">Price</param>
         public void RegisterItemPrice(Item item, decimal price)
         {
-            throw new NotImplementedException();
+            if (item == null)
+                throw new InvalidOperationException($"{nameof(item)} can't be null");
+
+            _pricesPerItem[item.Name] = new PricePerItem { Item = item, Price = price };
         }
 
+        /// <summary>
+        /// Registers bulk discount
+        /// </summary>
+        /// <param name="bulk">Bulk discount</param>
         public void SetBulkDiscount(BulkDiscount bulk)
         {
-            throw new NotImplementedException();
+            if (bulk == null)
+                throw new InvalidOperationException($"{nameof(bulk)} can't be null");
+            if (bulk.Item == null)
+                throw new InvalidOperationException($"{nameof(bulk.Item)} can't be null");
+
+            _bulkDiscounts[bulk.Item.Name] = bulk;
         }
     }
 }
